0a9559442cfb5c047034840386f7b2a71798dadc
Tim Pease <tim.pease@gmail.com>
2011-11-17 11:10:34 -0700

    updating to latest version of rspec

--- spec/little-plugger_spec.rb
+++ spec/little-plugger_spec.rb
@@ -4,22 +4,22 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 describe LittlePlugger do
 
   it "converts a string from camel-case to underscore" do
-    LittlePlugger.underscore('FooBarBaz').should == 'foo_bar_baz'
-    LittlePlugger.underscore('CouchDB').should == 'couch_db'
-    LittlePlugger.underscore('FOOBar').should == 'foo_bar'
-    LittlePlugger.underscore('Foo::Bar::BazBuz').should == 'foo/bar/baz_buz'
+    LittlePlugger.underscore('FooBarBaz').should be == 'foo_bar_baz'
+    LittlePlugger.underscore('CouchDB').should be == 'couch_db'
+    LittlePlugger.underscore('FOOBar').should be == 'foo_bar'
+    LittlePlugger.underscore('Foo::Bar::BazBuz').should be == 'foo/bar/baz_buz'
   end
 
   it "generates a default plugin path" do
-    LittlePlugger.default_plugin_path(LittlePlugger).should == 'little_plugger/plugins'
-    LittlePlugger.default_plugin_path(Spec::Runner).should == 'spec/runner/plugins'
+    LittlePlugger.default_plugin_path(LittlePlugger).should be == 'little_plugger/plugins'
+    LittlePlugger.default_plugin_path(Process::Status).should be == 'process/status/plugins'
   end
 
   it "generates a default plugin module" do
-    LittlePlugger.default_plugin_module('little_plugger').should == LittlePlugger
+    LittlePlugger.default_plugin_module('little_plugger').should be == LittlePlugger
     lambda {LittlePlugger.default_plugin_module('little_plugger/plugins')}.
         should raise_error(NameError, 'uninitialized constant LittlePlugger::Plugins')
-    LittlePlugger.default_plugin_module('spec/runner').should == Spec::Runner
+    LittlePlugger.default_plugin_module('process/status').should be == Process::Status
   end
 end
 
